4618b174f8f598c0b0e606562192a54016a3e3d4
Chad A <chad@neomantic.com>
2010-02-21 19:07:06 -0500

    Initial commit of bare android project for sdk version 6 (or Android 2.0.1)

---
+++ src/com/neomantic/sisceval/Eval.java
@@ -0,0 +1,13 @@
+package com.neomantic.sisceval;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Eval extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
